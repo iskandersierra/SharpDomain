f500e58980db3ef549c8fe827fa33dddb7185039
iskandersierra <iskander.sierra.zaldivar@gmail.com>
2014-11-29 15:41:53 +0100

    Added new CoreDomains folder and UserManagement assemblies Added new MassTransitAdapter project to SharpDomain framework for implementing Message Bus

---
+++ Applications/TestMVCApp/Areas/UserManagement/Controllers/UserRoleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMVCApp.Areas.UserManagement.Controllers
+{
+    public class UserRoleController : Controller
+    {
+        // GET: UserManagement/UserRole
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Domains/CoreDomains/UserManagement/CoreDomains.UserManagement/RoleDomain/IRoleCommands.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreDomains.UserManagement.RoleDomain
+{
+    public interface IRoleCommands
+    {
+    }
+}
