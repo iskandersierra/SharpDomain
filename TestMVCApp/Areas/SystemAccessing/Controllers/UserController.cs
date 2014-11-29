177dbbfecf03b83babbdc8d882955085e4c21a89
iskandersierra <iskander.sierra.zaldivar@gmail.com>
2014-11-29 15:09:56 +0100

    Removed previous core domains to start over

---
+++ TestMVCApp/Areas/SystemAccessing/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SharpDomain.Messaging;
+
+namespace TestMVCApp.Areas.SystemAccessing.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ICommandBus commandBus;
+        //private readonly UserQueries userQueries;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
